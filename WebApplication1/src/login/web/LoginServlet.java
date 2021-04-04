93d4ba48db66ecf697a7b83ffffbd2f0cea3f298
Al-fi-ya <shaikh.21810814@viit.ac.in>
2021-04-04 15:54:01 +0530

    Login JSP Pages Added

--- WebApplication1/src/login/web/LoginServlet.java
+++ WebApplication1/src/login/web/LoginServlet.java
@@ -4,13 +4,19 @@
  * and open the template in the editor.
  */
 
+package src.login.web;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import src.login.bean.LoginBean;
+import src.login.database.LoginDao;
 
 /**
  *
@@ -18,6 +24,37 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID =1;
+    private LoginDao loginDao;
+    
+    public void init()
+    {
+        loginDao = new LoginDao();
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+        String loginId = request.getParameter("loginId");
+        String password = request.getParameter("password");
+        LoginBean loginBean = new LoginBean();
+        loginBean.setLoginId(loginId);
+        loginBean.setPassword(password);
+
+        try {
+            if (loginDao.validate(loginBean)) {
+                //HttpSession session = request.getSession();
+                // session.setAttribute("username",username);
+                response.sendRedirect("loginsuccess.jsp");
+            } else {
+                HttpSession session = request.getSession();
+                //session.setAttribute("user", username);
+                //response.sendRedirect("login.jsp");
+            }
+        }catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
---
+++ WebApplication1/web/WEB-INF/loginsuccess.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : loginsuccess
+    Created on : 03-Apr-2021, 4:38:10 PM
+    Author     : Alfiya
+--%>
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+ pageEncoding="ISO-8859-1"%>
+<%@page import="net.javaguides.login.database.*"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>CDAC PREKSHA NEW WEB APPLICATION loginsuccess</title>
+</head>
+<body>
+ <div align="center">
+  <h1>Login successful.</h1>
+ </div>
+</body>
+</html>
